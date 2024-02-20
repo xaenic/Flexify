3cf638392c9ea97a9a2011856c8f739f5d773d3a
Kiwi <allanvillegas35@gmail.com>
2024-02-20 20:54:42 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Flexify.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
