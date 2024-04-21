d6c35e9ce49da94e6e0d14c688f0dcdf2fd8cdc2
Kiwi <allanvillegas35@gmail.com>
2024-04-21 18:54:19 +0800

    test

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using Flexify.Models;
 using Microsoft.AspNetCore.Mvc;
 using NuGet.Protocol.Core.Types;
+using System.Data.SqlClient;
 
 namespace Flexify.Controllers
 {
@@ -23,10 +24,48 @@ namespace Flexify.Controllers
         public ViewResult Register(UserModel user)
         {
             if(!ModelState.IsValid) {
-
                 return View();
             }
+            string? connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
 
+            if(connectionString == null ) { 
+                return View("Error");
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@FirstName", firstName);
+                    command.Parameters.AddWithValue("@LastName", lastName);
+                    command.Parameters.AddWithValue("@Email", email);
+                    command.Parameters.AddWithValue("@Password", password);
+
+                    try
+                    {
+                        connection.Open();
+                        int rowsAffected = command.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            lblError.Text = "Registration successful!";
+                            lblError.ForeColor = System.Drawing.Color.Green;
+                            lblError.Visible = true;
+                        }
+                        else
+                        {
+                            lblError.Text = "Failed to register user.";
+                            lblError.ForeColor = System.Drawing.Color.Red;
+                            lblError.Visible = true;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        lblError.Text = "An error occurred: " + ex.Message;
+                        lblError.ForeColor = System.Drawing.Color.Red;
+                        lblError.Visible = true;
+                    }
+                }
+            }
             return View();
 
             
